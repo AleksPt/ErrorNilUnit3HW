14246b9bced09e0771449a336377e3f28c447cb0
Alex Petrenko <alex.pt@ya.ru>
2024-03-01 15:56:57 +0300

    Initial Commit

---
+++ ErrorNilUnit3HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ErrorNilUnit3HW
+//
+//  Created by Алексей on 01.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
