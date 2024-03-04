4647c6fc6ff75de5404e92d3a5e53d50bbfb9020
Alex Petrenko <alex.pt@ya.ru>
2024-03-04 23:23:43 +0300

    complete

--- Homework2/Controller/ViewController.swift
+++ Homework2/Controller/ViewController.swift
@@ -5,33 +5,65 @@ final class ViewController: UIViewController {
     private let networkManager = NetworkManager()
     private var news: [Articles]? = []
     
+    private lazy var searchController: UISearchController = {
+        $0.searchResultsUpdater = self
+        $0.searchBar.placeholder = "Поиск"
+        return $0
+    }(UISearchController(searchResultsController: nil))
+    
     private lazy var tableView: UITableView = {
         $0.dataSource = self
+        $0.register(NewsCell.self, forCellReuseIdentifier: NewsCell.reuseId)
         return $0
-    }(UITableView(frame: view.bounds, style: .insetGrouped))
+    }(UITableView(frame: view.bounds))
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.addSubview(tableView)
         
-        networkManager.getNews(keyword: "Apple") { [weak self] data in
-            self?.news = data
-            
-            DispatchQueue.main.async {
-                <#code#>
-            }
-        }
+        title = "Новости"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.searchController = searchController
+        
+        self.tableView.separatorStyle = .none
     }
-
-
 }
 
+// MARK: - UITableViewDataSource
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         news?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: NewsCell.reuseId, for: indexPath) as! NewsCell
+        
+        let cellItem = news?[indexPath.row]
+        
+        cell.configureCell(
+            title: cellItem?.title ?? "",
+            description: cellItem?.description ?? "",
+            imageUrl: cellItem?.urlToImage ?? ""
+        )
+        
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+}
+
+//MARK: - UISearchResultsUpdating
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            networkManager.getNews(keyword: searchText) { [weak self] data in
+                self?.news = data
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            }
+        }
     }
 }
