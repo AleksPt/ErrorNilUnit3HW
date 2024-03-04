329268f27f42b03c1951ddcbcf5d08c90d762325
Alex Petrenko <alex.pt@ya.ru>
2024-03-04 22:55:58 +0300

    create tableView

--- Homework2/Controller/ViewController.swift
+++ Homework2/Controller/ViewController.swift
@@ -2,11 +2,36 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    private let networkManager = NetworkManager()
+    private var news: [Articles]? = []
+    
+    private lazy var tableView: UITableView = {
+        $0.dataSource = self
+        return $0
+    }(UITableView(frame: view.bounds, style: .insetGrouped))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableView)
         
+        networkManager.getNews(keyword: "Apple") { [weak self] data in
+            self?.news = data
+            
+            DispatchQueue.main.async {
+                <#code#>
+            }
+        }
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        news?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
