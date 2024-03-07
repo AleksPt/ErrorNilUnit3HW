d0253956b47d030b6339ece73aab020d27bb96f3
Alex Petrenko <alex.pt@ya.ru>
2024-03-07 23:09:03 +0300

    complete NetworkManager

--- Photo/NetworkManager.swift
+++ Photo/NetworkManager.swift
@@ -2,5 +2,22 @@ import Foundation
 import Alamofire
 
 final class NetworkManager {
-    // https://api.unsplash.com/photos/?client_id=o1Qf3HRyR40Jm6N5pKkW923CJ7EbQAXgYl6LxnLq4U0
+    func getPhotos(completion: @escaping (Result<[Photo]?, Error>) -> ()) {
+        let parameters: Parameters = [
+            "client_id": "o1Qf3HRyR40Jm6N5pKkW923CJ7EbQAXgYl6LxnLq4U0",
+            "count": 10
+        ]
+        
+        AF.request("https://api.unsplash.com/photos/random", parameters: parameters).response { result in
+            guard result.error == nil else {
+                completion(.failure(result.error!))
+                return
+            }
+            
+            if let data = result.data {
+                let photos = try? JSONDecoder().decode([Photo].self, from: data)
+                completion(.success(photos))
+            }
+        }
+    }
 }
