59fc50574e6330c724624b81d3233610f00ed2e5
Alex Petrenko <alex.pt@ya.ru>
2024-03-13 17:39:32 +0300

    rename complete

--- Photo/FavoritesPhotoViewController.swift
+++ Photo/FavoritesPhotoViewController.swift
@@ -6,30 +6,96 @@ final class FavoritesPhotoViewController: UIViewController {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.contentMode = .scaleAspectFill
         $0.clipsToBounds = true
+        $0.layer.cornerRadius = 15
         return $0
     }(UIImageView())
+    
+    lazy var deleteImageButton: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setImage(UIImage(systemName: "trash"), for: .normal)
+        $0.tintColor = .red
+        return $0
+    }(UIButton(type: .custom, primaryAction: deleteImageAction))
+    
+    lazy var deleteImageAction = UIAction { [weak self] _ in
+        StorageManager.shared.deleteFolder()
+        self?.favoriteImageView.image = nil
+        self?.deleteImageButton.isHidden = true
+        self?.renameDirectoryButton.isHidden = true
+    }
+    
+    lazy var renameDirectoryButton: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.setImage(UIImage(systemName: "highlighter"), for: .normal)
+        $0.tintColor = .systemBlue
+        return $0
+    }(UIButton(type: .custom, primaryAction: renameDirectoryAction))
+    
+    lazy var renameDirectoryAction = UIAction { [weak self] _ in
+        self?.showAlert()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .dark
         view.addSubview(favoriteImageView)
-        
+        view.addSubview(deleteImageButton)
+        view.addSubview(renameDirectoryButton)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        if let imgData = StorageManager.shared.getImage(imgName: "image.jpeg") {
-            favoriteImageView.image = UIImage(data: imgData)
+        if let imageData = StorageManager.shared.getImage(imgName: "image.jpeg") {
+            favoriteImageView.image = UIImage(data: imageData)
         }
         
         NSLayoutConstraint.activate([
             favoriteImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             favoriteImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            favoriteImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            favoriteImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            favoriteImageView.heightAnchor.constraint(equalToConstant: 500)
+            favoriteImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            favoriteImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            favoriteImageView.heightAnchor.constraint(equalToConstant: view.frame.width),
+            
+            deleteImageButton.topAnchor.constraint(equalTo: favoriteImageView.bottomAnchor, constant: 10),
+            deleteImageButton.trailingAnchor.constraint(equalTo: favoriteImageView.trailingAnchor),
+            deleteImageButton.widthAnchor.constraint(equalToConstant: 40),
+            deleteImageButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            renameDirectoryButton.topAnchor.constraint(equalTo: favoriteImageView.bottomAnchor, constant: 10),
+            renameDirectoryButton.leadingAnchor.constraint(equalTo: favoriteImageView.leadingAnchor),
+            renameDirectoryButton.widthAnchor.constraint(equalToConstant: 40),
+            renameDirectoryButton.heightAnchor.constraint(equalToConstant: 40)
         ])
-       
+        
+        if favoriteImageView.image != nil {
+            deleteImageButton.isHidden = false
+            renameDirectoryButton.isHidden = false
+        } else {
+            deleteImageButton.isHidden = true
+            renameDirectoryButton.isHidden = true
+        }
+    }
+}
+
+extension FavoritesPhotoViewController {
+    func showAlert() {
+        let renameAlert = UIAlertController(
+            title: nil,
+            message: "Enter name for new folder:",
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(
+            title: "Ok",
+            style: .default) { action in
+                if let text = renameAlert.textFields?.first?.text {
+                    StorageManager.shared.moveFile(path: text)
+                }
+            }
+        
+        renameAlert.addTextField()
+        renameAlert.addAction(okAction)
+        present(renameAlert, animated: true)
     }
 }
--- Photo/StorageManager.swift
+++ Photo/StorageManager.swift
@@ -3,7 +3,9 @@ import Foundation
 class StorageManager{
 
     static let shared = StorageManager()
-        
+    
+    private var defaultPath = "photo"
+    
     func load(url: URL) {
         DispatchQueue.global().async { [weak self] in
             if let data = try? Data(contentsOf: url) {
@@ -20,7 +22,7 @@ class StorageManager{
     
     func saveImage(data: Data){
        var path = getPath()
-        path.append(path: "photo")
+        path.append(path: defaultPath)
         
         try? FileManager.default.createDirectory(at: path, withIntermediateDirectories: true)
         
@@ -31,16 +33,34 @@ class StorageManager{
         } catch {
             print(error.localizedDescription)
         }
-        
     }
-    
     
     func getImage(imgName: String) -> Data?{
         var path = getPath()
-        path.append(path: "photo")
+        path.append(path: defaultPath)
         path.append(path: imgName)
         
         return try? Data(contentsOf: path)
     }
     
+    func deleteFolder() {
+        var path = getPath()
+        path.append(path: defaultPath)
+        try? FileManager.default.removeItem(at: path)
+    }
+    
+    func moveFile(path: String) -> Bool {
+        do {
+            var oldPath = getPath()
+            oldPath.append(path: defaultPath)
+            var newPath = getPath()
+            newPath.append(path: path)
+            try FileManager.default.moveItem(at: oldPath, to: newPath)
+            deleteFolder()
+            defaultPath = path
+            return true
+        } catch {
+            return false
+        }
+    }
 }
